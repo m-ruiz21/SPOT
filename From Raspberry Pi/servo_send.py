a2dfd373fecfe6618feba358e9d360bbb4e1aca1
raspberry pi <raspberry@pi.com>
2024-03-21 16:30:33 -0500

    make servo_send into a module and have it acquire the serial connection

--- From Raspberry Pi/servo_send.py
+++ From Raspberry Pi/servo_send.py
@@ -1,22 +1,31 @@
 import serial
-import time
+import subprocess
 
-ser = serial.Serial('/dev/ttyACM1', 9600)  # Change 'COM3' to match your Arduino's serial port
-# Open the serial port
+def acquire_serial():
+	# look for /dev/ttyACM* devices
+	tty = subprocess.check_output("ls /dev/ttyACM* | head", shell=True, text=True)
+
+	# remove trailing whitespace
+	tty = tty.rstrip()
+
+	if tty == '':
+		raise Exception("Cannot acquire serial connection!")
 
 
-while True:
+	return serial.Serial(tty, 9600)
+
+ser = acquire_serial()
+
+def servo_send(pwm):
 	try:
+		# Open the serial port
 		if not ser.isOpen():
 			ser.open()
 
-		num_times = int(input("Enter PWM Value: "))
-		bytes = (str(num_times)+"\n").encode()
-		print('bytes:', bytes);
+		# pwm = int(input("Enter PWM Value: "))
+		bytes = (str(pwm)+"\n").encode()
 		ser.write(bytes)
-		print(f"Sent PWM value: {num_times}")
-
-		# ser.read()
+		print(f"Sent PWM value: {pwm}")
 
 	except Exception as e:
 		print(f"Error: {e}")
