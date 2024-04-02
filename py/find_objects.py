b7ecb856e213253a54b0a7bd75e70fd594679d1e
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2024-04-02 17:49:14 -0500

    updates

--- py/find_objects.py
+++ py/find_objects.py
@@ -1,7 +1,27 @@
-import cv2
-from realsense_camera import *
-from mask_rcnn import *
+# create python script to use rplidar to find objects
 
-# Load Realsense camera and Mask R-CNN
-rs = RealsenseCamera()
-mrcnn = MaskRCNN()
+import sys
+import time
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from rplidar import RPLidar
+
+lidar = RPLidar('/COM6')
+
+print("kaka")
+info = lidar.get_info()
+print(info)
+print("poo poo kaka")
+
+health = lidar.get_health()
+print(health)
+
+for i, scan in enumerate(lidar.iter_scans()):
+    print('%d: Got %d measurments' % (i, len(scan)))
+    if i > 10:
+        break
+
+lidar.stop()
+lidar.stop_motor()
+lidar.disconnect()
--- py/map.py
+++ py/map.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 import pyrealsense2 as rs
+import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 # Initialize RealSense pipeline
 pipeline = rs.pipeline()
@@ -10,38 +13,41 @@ config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 # Start streaming
 pipeline.start(config)
 
+fig, ax = plt.subplots()
+
+def update(i):
+    # Wait for a new depth frame
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+
+    if not depth_frame:
+        return
+
+    # Convert depth frame to numpy array
+    depth_image = np.asanyarray(depth_frame.get_data())
+
+    # scale depth image to cm instead of mm
+    depth_scaled = depth_image * 0.1
+    # find the maximum depth value
+    max_depth = np.max(depth_scaled)
+
+    depth_map = np.zeros((640, 640), np.uint8)
+
+    # find width and height of the image
+    for y in range(220, 300):
+        for x in range(640):
+            depth_index = int(depth_scaled[y, x]) - 1 
+            if depth_index < 200:
+                depth_map[depth_index, x] = 1
+
+
+    ax.clear()
+    ax.imshow(depth_map, cmap='gray_r', origin='lower')
+
+ani = FuncAnimation(fig, update, interval=100)
+
 try:
-    while True:
-        # Wait for a new depth frame
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-
-        if not depth_frame:
-            continue
-
-        # Convert depth frame to numpy array
-        depth_image = np.asanyarray(depth_frame.get_data())
-
-        # scale depth image to cm instead of mm
-        depth_scaled = depth_image * 0.1
-        # find the maximum depth value
-        max_depth = np.max(depth_scaled)
-
-        # create empty array that's 6500x640
-        depth_map = np.zeros((640, 640), np.uint8)
-
-        # find width and height of the image
-        for y in range(220, 300):
-            for x in range(640):
-                depth_index = int(depth_scaled[y, x]) - 1 
-                if depth_index < 200:
-                    depth_map[depth_index, x] = 1
-    
-        import matplotlib.pyplot as plt
-        plt.imshow(depth_map, cmap='gray_r', origin='lower')
-        plt.show()
-
-
+    plt.show()
 finally:
     pipeline.stop()
     cv2.destroyAllWindows()
