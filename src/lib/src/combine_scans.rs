db0be9acb3ded2f46eacf806d6550333b42abf8d
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2024-04-04 20:01:29 -0500

    combine scans .rs

--- src/lib/src/combine_scans.rs
+++ src/lib/src/combine_scans.rs
@@ -16,12 +16,13 @@ pub fn combine_grid(occupancy_grids: Vec<OccupancyGrid>) {
     let mut combined_occupancy_grid = vec![vec![0.0; y_width]; x_width];
 
     for grid in occupancy_grids {
+        let x_offset = (x_width - grid.x_width) / 2;
         for x in 0..grid.x_width {
             for y in 0..grid.y_width {
-                grid.occupancy_map[x][y] = combined_occupancy_grid[x][y];
+                combined_occupancy_grid[x + x_offset][y] = grid.occupancy_map[x][y];
             }
         }
-    };
+    }
 
     combined_occupancy_grid
 }
